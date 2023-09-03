9c1b48b7985450b35cdea56c3aa023743e009810
Jeffrey Farías <jeffreyfarias5@gmail.com>
2023-09-03 17:33:52 -0300

    Terminado, solo falta sbir el video

--- src/components/MiApi.jsx
+++ src/components/MiApi.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react';
 
-function MiApi({area,filtro }) {
+function MiApi({ area, filtro }) {
   const [recipes, setRecipes] = useState([]);
-
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
     fetchRecipe();
   }, [area]);
@@ -10,29 +10,35 @@ function MiApi({area,filtro }) {
 
 
   const fetchRecipe = async () => {
+    setIsLoading(true);
     try {
       const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${area}`);
       const data = await response.json();
       if (data.meals) {
-        setRecipes(data.meals);
+        const sortedMeals = data.meals.sort((a, b) => a.strMeal.localeCompare(b.strMeal));
+        setRecipes(sortedMeals);
       }
     } catch (error) {
       console.error("Hubo un error al buscar la receta:", error);
+    } finally {
+      setIsLoading(false);  
     }
   };
 
   const filteredRecipes = recipes.filter(recipe => recipe.strMeal.toLowerCase().includes(filtro.toLowerCase()));
   return (
     <div className='row'>
-      {filteredRecipes.length > 0 ? (
+      {isLoading ? (
+        <p>Cargando...</p>
+      ) : filteredRecipes.length > 0 ? (
         filteredRecipes.map(recipe => (
-          <div key={recipe.idMeal} className='col-4'>
+          <div key={recipe.idMeal} className='col-lg-4 col-md-6 col-sm-12 mt-3'>
             <h4>{recipe.strMeal}</h4>
-            <img className="meal-image" src={recipe.strMealThumb} alt={recipe.strMeal} />
+            <img className="meal-image img-fluid" src={recipe.strMealThumb} alt={recipe.strMeal} />
           </div>
         ))
       ) : (
-        <p>Cargando recetas...</p>
+        <p>No se encontraron recetas</p>
       )}
     </div>
   );
